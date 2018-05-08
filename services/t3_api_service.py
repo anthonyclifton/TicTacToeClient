7e24f5ee62abe7eef679d52476933815274f5f87
Anthony Clifton <anthony@changedearth.com>
2018-05-07 19:02:06 -0500

    A bunch of ugly spike code for pinging the server, will be removed mostly

--- services/t3_api_service.py
+++ services/t3_api_service.py
@@ -3,8 +3,8 @@ import requests
 
 
 class T3ApiService(object):
-    def __init__(self):
-        pass
+    def __init__(self, base_url):
+        self.base_url = base_url
 
     def create_game(self):
         pass
@@ -15,10 +15,19 @@ class T3ApiService(object):
     def mark_cell(self):
         pass
 
-    def generic_post(self):
+    def ping(self):
+        url = 'http://localhost:3334/ping'
+        payload = {
+            'myurl': 'my own url'
+        }
+        self.generic_post(url, payload)
+
+    def generic_get(self, url):
+        pass
+
+    def generic_post(self, url, payload):
         # base_url = "www.server.com"
         # final_url = "/{0}/friendly/{1}/url".format(base_url, uuid4())
         #
         # payload = {'number': 2, 'value': 1}
-        # response = requests.post(final_url, data=payload)
-        pass
+        requests.post(url, data=payload)
--- t3client.py
+++ t3client.py
@@ -1,18 +1,43 @@
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from flask import Flask, request
 import argparse
 import json
 
+from services.t3_api_service import T3ApiService
+
+t3_api_service = T3ApiService('some-base-url')
+
 app = Flask(__name__)
 
 
+def ping():
+    print("Pinging server")
+    t3_api_service.ping()
+
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=ping,
+    trigger=IntervalTrigger(seconds=5),
+    id='ping_server',
+    name='Ping server every five seconds',
+    replace_existing=True)
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+
 def start():
-    app.run()
+    pass
 
 
-@app.route('/', methods=['POST'])
-def foo():
+@app.route('/pong', methods=['POST'])
+def update():
     data = json.loads(request.data)
-    print "New commit by: {}".format(data['commits'][0]['author']['name'])
+    print("Pong from server {}".format(data["pong"]))
     return "OK"
 
 
@@ -26,3 +51,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args.func()
 
+    app.run(host='0.0.0.0', port=3333)
