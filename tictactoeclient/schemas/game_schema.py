9e386fad42d57acd4c9ac4645db04726cceadf19
Anthony Clifton <anthony@changedearth.com>
2018-05-13 00:41:15 -0500

    Changed numbers to integers in schema

--- tictactoeclient/schemas/game_schema.py
+++ tictactoeclient/schemas/game_schema.py
@@ -2,9 +2,9 @@ from marshmallow import Schema, fields
 
 
 class MarkSchema(Schema):
-    x = fields.Number()
-    y = fields.Number()
-    value = fields.Number()
+    x = fields.Integer()
+    y = fields.Integer()
+    value = fields.Integer()
 
 
 class PlayerSchema(Schema):
@@ -15,10 +15,10 @@ class PlayerSchema(Schema):
 class GameSchema(Schema):
     name = fields.String()
     key = fields.UUID()
-    size_x = fields.Number()
-    size_y = fields.Number()
+    size_x = fields.Integer()
+    size_y = fields.Integer()
     player_x = fields.Nested(PlayerSchema, required=False)
     player_o = fields.Nested(PlayerSchema, required=False)
     cells = fields.Nested(MarkSchema, many=True)
-    winning_length = fields.Number()
+    winning_length = fields.Integer()
     state = fields.Integer()
