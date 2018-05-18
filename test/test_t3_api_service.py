df0ea596eca06f40a84398ca5d2ecad49eb5e206
Anthony Clifton <anthony@changedearth.com>
2018-05-18 14:19:42 -0500

    Added test for t3 api service create game method

---
+++ test/test_t3_api_service.py
@@ -0,0 +1,30 @@
+import unittest
+
+from mock import patch, Mock
+from requests import Response
+
+from tictactoeclient.services.t3_api_service import T3ApiService
+
+
+class TestT3ApiService(unittest.TestCase):
+    @patch('tictactoeclient.services.t3_api_service.requests')
+    def test__create_game__calls_post_with_game_size(self, mock_requests):
+        t3_api_service = T3ApiService('base')
+
+        mock_response = Mock()
+        # mock_response.text = 'mock return'
+        mock_response.content = '{}'
+
+        mock_requests.post.return_value = mock_response
+
+        t3_api_service.create_game("Test Game", "Player Name", "Update URL")
+
+        call_args = mock_requests.post.call_args
+
+        expected_json = {
+            'update_url': 'Update URL',
+            'player_name': 'Player Name',
+            'game_name': 'Test Game'
+        }
+
+        self.assertEqual(expected_json, call_args[1]['json'])
